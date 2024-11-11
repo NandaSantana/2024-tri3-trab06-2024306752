f3dcc5d62bfb57898d357eada96949223e976286
NandaSantana <nandasbbs9@gmail.com>
2024-11-11 15:21:06 -0300

    cikdfjiqgjr

---
+++ assets/chat.js
@@ -0,0 +1,29 @@
+document.getElementById('btn').addEventListener('click', function() {
+    const input = document.getElementById('chatInput');
+    const messageText = input.value.trim();
+  
+
+  
+    const messageContainer = document.createElement('div');
+    messageContainer.classList.add('message', 'owner'); 
+    
+    const messageContent = document.createElement('div');
+    messageContent.classList.add('content');
+    messageContent.textContent = messageText;
+  
+    const messageTime = document.createElement('div');
+    messageTime.classList.add('time');
+    const currentTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+    messageTime.textContent = currentTime;
+  
+    messageContainer.appendChild(messageContent);
+    messageContainer.appendChild(messageTime);
+  
+    const chatMain = document.querySelector('main');
+    chatMain.appendChild(messageContainer);
+  
+    input.value = '';
+    input.focus();
+  
+    chatMain.scrollTop = chatMain.scrollHeight;
+  });
