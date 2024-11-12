cb5e41ce2ae18d93df8423643ce46aa67af061cb
NandaSantana <nandasbbs9@gmail.com>
2024-11-12 14:36:12 +0000

    cbjxcv bndf

--- assets/chat.js
+++ assets/chat.js
@@ -1,4 +1,4 @@
-document.getElementById('btn').addEventListener('click', function() {
+/*document.getElementById('btn').addEventListener('click', function() {
     const input = document.getElementById('chatInput');
     const messageText = input.value.trim();
   
@@ -29,4 +29,35 @@ document.getElementById('btn').addEventListener('click', function() {
     input.focus();
   
     chatMain.scrollTop = chatMain.scrollHeight;
-  });
+  });*/
+
+  const main = document.querySelector("main")
+    const input = document.querySelector("input")
+    const button = document.querySelector("button")
+
+    function monstrarMensagem(owner, message, timestamp, nick) {
+      main.innerHTML += `
+        <div class="message ${owner ? "owner" : ""}">
+          <div class="content">${message}</div>
+          <div class="time">${timestamp}</div>
+          <div class="nick">${nick}</div>
+        </div>
+      `
+    }
+
+    const ws = new WebSocket("ws://192.168.120.53:4000")
+  
+    ws.addEventListener("open", () => console.log("Conectado"))
+    ws.addEventListener("close", () => console.log("Desconectado"))
+
+    ws.addEventListener("message", (event) => {
+      const data = JSON.parse(event.data)
+      monstrarMensagem(false, data.message, data.timestamp, data.nick)
+    })
+
+    button.addEventListener("click", () => {
+      const message = input.value
+      ws.send(message)
+      input.value = ""
+    })
+  
